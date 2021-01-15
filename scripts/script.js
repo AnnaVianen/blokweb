012ad1bd96f8eceebd8626c0b7046e2c1f72dab6
AnnaVianen <74233451+AnnaVianen@users.noreply.github.com>
2021-01-15 13:17:31 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
 // JavaScript Document
+
+
+
+
+
+function myFunction(){
+    var x = document.getElementById("myLinks");
+    if (x.style.display === "block") {
+        x.style.display = "none";
+    }
+    else {
+        x.style.display = "block";
+    }
+}
